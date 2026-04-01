46be76fd01f99be75bef728e13722b0237b0f3e3
ilhyeonchu <ilhyeonchu@gmail.com>
2026-04-01 19:10:16 +0900

    fin 2-4

---
+++ code/2-4/2-4.c
@@ -0,0 +1,15 @@
+#include <ctype.h>
+#include <stdio.h>
+
+int main() {
+  char chars[] = {'A', 'z', '5', '1', ' '};
+  int n = sizeof(chars) / sizeof(chars[0]);
+
+  for (int i = 0; i < n; i++) {
+    char ch = chars[i];
+    printf("%c - alpha:%d digit:%d upper:%c lower:%c\n", ch, isalpha(ch),
+           isdigit(ch), toupper(ch), tolower(ch));
+  }
+
+  return 0;
+}
